0d61afc59987afd7577460eb4d3ef920050b9097
Rafael Fernandes <dvr.rafa@gmail.com>
2024-01-05 16:05:17 -0300

    comunicacao sincrona entre pagamento e pedido client

--- pagamentos/src/main/java/br/com/dvrfood/pagamentos/service/PagamentoService.java
+++ pagamentos/src/main/java/br/com/dvrfood/pagamentos/service/PagamentoService.java
@@ -1,6 +1,7 @@
 package br.com.dvrfood.pagamentos.service;
 
 import br.com.dvrfood.pagamentos.dto.PagamentoDto;
+import br.com.dvrfood.pagamentos.http.PedidoClient;
 import br.com.dvrfood.pagamentos.model.Pagamento;
 import br.com.dvrfood.pagamentos.model.Status;
 import br.com.dvrfood.pagamentos.repository.PagamentoRepository;
@@ -11,12 +12,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PagamentoService {
     @Autowired
     private PagamentoRepository repository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private PedidoClient pedido;
 
     public Page<PagamentoDto> obterTodos(Pageable paginacao) {
         return repository
@@ -48,6 +53,18 @@ public class PagamentoService {
 
     public void excluirPagamento(Long id) {
         repository.deleteById(id);
+    }
+
+    public void confirmarPagamento(Long id){
+        Optional<Pagamento> pagamento = repository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO);
+        repository.save(pagamento.get());
+        pedido.atualizaPagamento(pagamento.get().getPedidoId());
     }
 
 }
